d520abb51290b05757badbcdda8f4c232ff3a439
Vadim Pashkov <vadimpashkov.job@gmail.com>
2025-11-30 19:25:35 +0300

    Edit symbols on int

--- config/unicode_symbols.h
+++ config/unicode_symbols.h
@@ -1,36 +1,36 @@
 #pragma once
 
-#define UC_SB_DQT         0x0022    0      // "
-#define UC_SB_SQT         0x0027    0      // '
-#define UC_SB_GRAVE       0x0060    0      // `
+#define UC_SB_DQT         34     0      // "
+#define UC_SB_SQT         39     0      // '
+#define UC_SB_GRAVE       96     0      // `
 
-#define UC_SB_HASH        0x0023    0      // #
-#define UC_SB_NO          0x2116    0      // №
+#define UC_SB_HASH        35     0      // #
+#define UC_SB_NO          8470   0      // №
 
-#define UC_SB_AT          0x0040    0      // @
-#define UC_SB_DLLR        0x0024    0      // $
-#define UC_SB_CARET       0x005E    0      // ^
+#define UC_SB_AT          64     0      // @
+#define UC_SB_DLLR        36     0      // $
+#define UC_SB_CARET       94     0      // ^
 
-#define UC_SB_SEMI        0x003B    0      // ;
-#define UC_SB_COLON       0x003A    0      // :
+#define UC_SB_SEMI        59     0      // ;
+#define UC_SB_COLON       58     0      // :
 
-#define UC_SB_AMPS        0x0026    0      // &
-#define UC_SB_QMARK       0x003F    0      // ?
+#define UC_SB_AMPS        38     0      // &
+#define UC_SB_QMARK       63     0      // ?
 
-#define UC_SB_LBKT        0x005B    0      // [
-#define UC_SB_RBKT        0x005D    0      // ]
+#define UC_SB_LBKT        91     0      // [
+#define UC_SB_RBKT        93     0      // ]
 
-#define UC_SB_LBRC        0x007B    0      // {
-#define UC_SB_RBRC        0x007D    0      // }
+#define UC_SB_LBRC        123    0      // {
+#define UC_SB_RBRC        125    0      // }
 
-#define UC_SB_LT          0x003C    0      // <
-#define UC_SB_GT          0x003E    0      // >
+#define UC_SB_LT          60     0      // <
+#define UC_SB_GT          62     0      // >
 
-#define UC_SB_COMMA       0x002C    0      // ,
-#define UC_SB_DOT         0x002E    0      // .
+#define UC_SB_COMMA       44     0      // ,
+#define UC_SB_DOT         46     0      // .
 
-#define UC_SB_FSLH        0x002F    0      // /
-#define UC_SB_BSLH        0x005C    0      // (backslash)
-#define UC_SB_PIPE        0x007C    0      // |
+#define UC_SB_FSLH        47     0      // /
+#define UC_SB_BSLH        92     0      // (backslash)
+#define UC_SB_PIPE        124    0      // |
 
-#define UC_SB_TILDE       0x007E    0      // ~
+#define UC_SB_TILDE       126    0      // ~
